e46afccc1be636ccb042c78907e71e522400a63a
polyqubit <polyqfive@gmail.com>
2025-02-16 15:42:16 -0800

    rng

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="">
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-      </main>
+    <div className="items-center justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-geist-mono)]">
+        <h1 className="text-xl text-center">
+            RNG Simulator Tycoon GO!
+        </h1>
+        <div>
+        
+        </div>
+        <div className="flex items-center border-4 border-white content-around gap-1 text-7xl">
+            <div className="text-red-500">S</div>
+            <div className="text-yellow-500">P</div>
+            <div className="text-green-500">I</div>
+            <div className="text-cyan-500">N</div>
+        </div>
     </div>
   );
 }
